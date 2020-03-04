ea9191fd5116e51d60af4b4540c020e0427e4deb
hugoleogs <hugoleonardo.gs2@gmail.com>
2020-03-04 12:53:52 -0300

    "Criado o metodo cumprimentar"

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,8 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return 'Sou Eu!'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p)) # Não é usual
+    print(p.cumprimentar())
