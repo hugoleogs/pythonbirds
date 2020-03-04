f888b572b42a54a4cdbfc44fe31255271e29f93f
hugoleogs <hugoleonardo.gs2@gmail.com>
2020-03-04 18:09:30 -0300

    "Criado atributo complexo filho"

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,21 @@
 class Pessoa:
 
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return 'Sou Eu!'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Hugo_Leonardo')
-    print(Pessoa.cumprimentar(p))  # Não é usual
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Hugo Gomes'
-    print(p.nome)
-    print(p.idade)
+    hugo = Pessoa(nome='Hugo_Leonardo')
+    luciano = Pessoa(hugo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))  # Não é usual
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
