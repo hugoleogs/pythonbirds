cabe2b7a1edae0708ad3cfb3c2a5b4f8c5c034d3
hugoleogs <hugoleonardo.gs2@gmail.com>
2020-03-04 13:04:29 -0300

    "Criado atributos idade e nome"

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,18 @@
 class Pessoa:
+
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return 'Sou Eu!'
 
+
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p)) # Não é usual
+    p = Pessoa('Hugo_Leonardo')
+    print(Pessoa.cumprimentar(p))  # Não é usual
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Hugo Gomes'
+    print(p.nome)
+    print(p.idade)
